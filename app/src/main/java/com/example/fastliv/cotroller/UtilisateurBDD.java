6b9fe086a60650c35b02e14fdbe1ee6004382189
Djily SARR <djilysarr275@gmail.com>
2023-12-19 01:24:24 +0100

    get form data and try to create collection

--- app/src/main/java/com/example/fastliv/cotroller/UtilisateurBDD.java
+++ app/src/main/java/com/example/fastliv/cotroller/UtilisateurBDD.java
@@ -4,12 +4,19 @@ import static android.content.ContentValues.TAG;
 
 import android.util.Log;
 
+
 import com.example.fastliv.model.Utilisateur;
+
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class UtilisateurBDD {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     // Create a new user with a first and last name
-
+    public void addUtilisateur(Utilisateur u){
+        db.collection("utilisateurs").document("user1")
+                .set(u)
+                .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot written with ID: " ))
+                .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
+    }
 }
--- app/src/main/java/com/example/fastliv/ui/Inscription.java
+++ app/src/main/java/com/example/fastliv/ui/Inscription.java
@@ -4,19 +4,84 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.fastliv.MainActivity;
 import com.example.fastliv.R;
+import com.example.fastliv.cotroller.UtilisateurBDD;
+import com.example.fastliv.model.Utilisateur;
 
-public class Inscription extends AppCompatActivity  {
+public class Inscription extends AppCompatActivity  implements View.OnClickListener, AdapterView.OnItemSelectedListener{
+    Button btnInscrire;
+    EditText inputEmail;
+    EditText inputPassword;
+    EditText inputPhone;
+    String inputRole;
+    EditText inputImmatriculation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
 
+        btnInscrire = findViewById(R.id.btn_inscrire);
+        btnInscrire.setOnClickListener(this);
 
+        Spinner sp = findViewById(R.id.inscrire_role);
+        sp.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this
+                , R.array.Roles, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
+
+        inputEmail = findViewById(R.id.inscrire_email);
+        inputPassword = findViewById(R.id.inscrire_password);
+        inputPhone = findViewById(R.id.inscrire_phone);
+        inputRole = findViewById(R.id.inscrire_role).toString();
+        inputImmatriculation = findViewById(R.id.inscrire_immatriculation);
+
+
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String[] choix = getResources().getStringArray(R.array.Roles);
+        inputRole = choix[position];
+        if (inputRole.equals("Chauffeur")) {
+            inputImmatriculation.setVisibility(View.VISIBLE);
+        }else {
+            inputImmatriculation.setVisibility(View.INVISIBLE);
+            inputImmatriculation.setText("");
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnInscrire) {
+            Log.d("success", inputEmail.getText().toString());
+            Log.d("success", inputPassword.getText().toString());
+            Log.d("success", inputPhone.getText().toString());
+            Log.d("success", inputRole);
+            Log.d("success", inputImmatriculation.getText().toString());
+
+            Utilisateur user = new Utilisateur(inputEmail.getText().toString(),
+                    inputPassword.getText().toString(), inputPhone.getText().toString(),
+                    inputRole, inputImmatriculation.getText().toString());
+
+            UtilisateurBDD userBDD = new UtilisateurBDD();
+            userBDD.addUtilisateur(user);
+        }
     }
 
 
